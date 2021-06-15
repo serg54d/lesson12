2f89d3de1714081e140d6675141b6aa9576981ec
Sergey <serg54d@bk.ru>
2021-06-16 02:12:59 +0300

    create some prompt and create object for recording into it values from prompt

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
 const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
 
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const lastViewFilms = prompt('Последний просмотренный фильм?', ''),
+      ratind = +prompt('На сколько оцените его?', '');
+      
+
+const lastViewFilms2 = prompt('Последний просмотренный фильм?', ''),
+      rating2 = +prompt('Насколько оцените его?', '');
+
+personalMovieDB.movies[lastViewFilms] = ratind;
+personalMovieDB.movies[lastViewFilms2] = rating2;
+
+console.log(personalMovieDB);
